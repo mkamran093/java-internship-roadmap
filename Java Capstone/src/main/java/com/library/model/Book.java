10cb12a647a300a0ef356070701bb3f976cfbe16
Muhammad Kamran <mkamran.bscssef20@iba-suk.edu.pk>
2026-08-20 00:40:41 +0500

    Capstone project start

---
+++ Java Capstone/src/main/java/com/library/model/Book.java
@@ -0,0 +1,80 @@
+package com.library.model;
+
+import java.util.Objects;
+
+public class Book {
+
+    private Long id;
+    private String title;
+    private String isbn;
+    private String author;
+    private boolean isAvailable;
+
+    public Book() {
+    }
+
+    public Book(Long id, String title, String isbn, String author, boolean isAvailable) {
+        this.id = id;
+        this.title = title;
+        this.isbn = isbn;
+        this.author = author;
+        this.isAvailable = isAvailable;
+    }
+
+
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getIsbn() {
+        return isbn;
+    }
+
+    public void setIsbn(String isbn) {
+        this.isbn = isbn;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public boolean isAvailable() {
+        return isAvailable;
+    }
+
+    public void setAvailable(boolean available) {
+        isAvailable = available;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        Book book = (Book) o;
+        return Objects.equals(isbn, book.isbn);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(isbn);
+    }
+
+    @Override
+    public String toString() {
+        return "Book{" +
+                "id=" + id +
+                ", title='" + title + '\'' +
+                ", isbn='" + isbn + '\'' +
+                ", author='" + author + '\'' +
+                ", isAvailable=" + isAvailable +
+                '}';
+    }
+}
